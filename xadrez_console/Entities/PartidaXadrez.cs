3f495bd8f0dd8938f0290b4f7be627bfae880907
Pedro Araujo <pe.araujo21@gmail.com>
2021-02-11 14:22:29 -0300

    Mudando nome do metodo para movimento possivel

--- xadrez_console/Entities/PartidaXadrez.cs
+++ xadrez_console/Entities/PartidaXadrez.cs
@@ -100,7 +100,7 @@ namespace xadrez_console.Entities
 
         public void ValidarPosicaoDestino(Posicao origem, Posicao destino)
         {
-            if (!Tabuleiro.Peca(origem).PodeMoverPara(destino))
+            if (!Tabuleiro.Peca(origem).MovimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posição de destino inválida!");
             }
--- xadrez_console/Entities/Peca.cs
+++ xadrez_console/Entities/Peca.cs
@@ -44,7 +44,7 @@ namespace xadrez_console.Entities
             return false;
         }
 
-        public bool PodeMoverPara(Posicao posicao)
+        public bool MovimentoPossivel(Posicao posicao)
         {
             return MovimentosPossiveis()[posicao.Linha, posicao.Coluna];
         }
